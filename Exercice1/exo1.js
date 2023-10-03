6fac29b8fff627c6f7678f4b7df5fe17e8a92276
Wagner Theo <wgr.theo@gmail.com>
2023-10-03 16:33:03 +0200

    Exercice1 / partie1

---
+++ Exercice1/exo1.js
@@ -0,0 +1,11 @@
+function calculatrice(nb1, sign, nb2)
+{
+    if (sign === "+")
+    return(nb1 + nb2)
+    if (sign === "-")
+    return(nb1 - nb2)
+    if (sign === "*")
+    return(nb1 * nb2)
+    if (sign === "/")
+    return(nb1 / nb2)
+}
